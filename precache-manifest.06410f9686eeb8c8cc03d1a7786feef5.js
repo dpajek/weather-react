93f3644a80e8e74fd13487db9a74a097549198a0
dpajek <dan.pajek@gmail.com>
2020-09-28 22:24:15 -0400

    Updates

---
+++ precache-manifest.06410f9686eeb8c8cc03d1a7786feef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee10e8b70d17c72809d11e54e8da8a7",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "2d87efa1a72a7b69e7d6",
+    "url": "/weather-react/static/css/main.0d8ee5ed.chunk.css"
+  },
+  {
+    "revision": "43f0b2bb382262100dcb",
+    "url": "/weather-react/static/js/2.72629be3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.72629be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d87efa1a72a7b69e7d6",
+    "url": "/weather-react/static/js/main.29b82acc.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  },
+  {
+    "revision": "2d5f0a549cf7da133a6a1f0c9c3f03ee",
+    "url": "/weather-react/static/media/sunny.2d5f0a54.jpg"
+  }
+]);
